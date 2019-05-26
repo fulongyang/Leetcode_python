0da06a61a406042686d7049408764db0d5b74b25
fulongyang <993294959@qq.com>
2019-05-26 21:02:58 +0800

    alter code

--- 基础算法/列表计数/count_list_v1.py
+++ 基础算法/列表计数/count_list_v1.py
@@ -30,9 +30,23 @@ def listCount3():
 
 
 
+def listCount4():
+    a = [1, 2, 3, 1, 1, 2]
+    b = {i:a.count(i) for i in a}
+    return b
 
+
+
+
+
+
+import timeit
 
 
 if __name__ == "__main__":
     # listCount2()
-    listCount3()
+    # listCount3()
+    # s = listCount4()
+    # print (timeit.timeit(stmt="listCount4", setup='from __main__ import listCount4',number=1))
+    print (timeit.timeit(stmt="[i for i in range(100)]" ,number=10000))
+    print (timeit.timeit(stmt="{i for i in range(100)}" ,number=10000))
