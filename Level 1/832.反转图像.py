91770d236bcaf60b6e762e443fc76da90db2e955
fulongyang <longlongtimeafter@gmail.com>
2019-09-08 00:47:55 +0800

    1.反转图片+1

--- Level 1/832.反转图像.py
+++ Level 1/832.反转图像.py
@@ -11,6 +11,9 @@ class Solution:
         #putput [[1,0,0],[0,1,0],[1,1,1]]
         #输入: [[1, 1, 0, 0], [1, 0, 0, 1], [0, 1, 1, 1], [1, 0, 1, 0]]
         #输出: [[1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 0, 1], [1, 0, 1, 0]]
+        '''
+            思路：循环数组，将每个数组的list反转，被反转的数组 1=0  0=1
+        '''
         result = list(map(lambda A:[a-1 if a ==1 else a+1 for a in A[::-1]],A))
         return result
 
@@ -22,11 +25,18 @@ class Solution:
         return result
 
 
+    @staticmethod
+    def flipAndInvertImage03(A):
+        return list(map(lambda x:list(map(lambda y:0 if y==1 else 1,x[::-1])),A))
+
+
+
 if __name__ == "__main__":
     # A = [[1, 1, 0], [1, 0, 1], [0, 0, 0]]
     A = [[1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 0, 1], [1, 0, 1, 0]]
-    Solution.flipAndInvertImage01(A)
-    Solution.flipAndInvertImage02(A)
+    Solution.flipAndInvertImage01(A)   #超过34%
+    Solution.flipAndInvertImage02(A)   #超过66%
+    Solution.flipAndInvertImage03(A)   #超过57%
 
 
 
