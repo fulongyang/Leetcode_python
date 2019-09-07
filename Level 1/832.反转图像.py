4fdeb1bcb142287d233356e054648559f455180a
fulongyang <longlongtimeafter@gmail.com>
2019-09-08 00:24:03 +0800

    1.反转图片算法 +2

--- Level 1/832.反转图像.py
+++ Level 1/832.反转图像.py
@@ -6,7 +6,7 @@
 class Solution:
     # def flipAndInvertImage(self, A: List[List[int]]) -> List[List[int]]:
     @staticmethod
-    def flipAndInvertImage(A):
+    def flipAndInvertImage01(A):
         #input  [[1,1,0],[1,0,1],[0,0,0]]
         #putput [[1,0,0],[0,1,0],[1,1,1]]
         #输入: [[1, 1, 0, 0], [1, 0, 0, 1], [0, 1, 1, 1], [1, 0, 1, 0]]
@@ -15,11 +15,18 @@ class Solution:
         return result
 
 
+    @staticmethod
+    def flipAndInvertImage02(A):
+        alter = lambda x: 1 if x==0 else 0
+        result = [[alter(b) for b in a[::-1]] for a in A]
+        return result
+
 
 if __name__ == "__main__":
     # A = [[1, 1, 0], [1, 0, 1], [0, 0, 0]]
     A = [[1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 0, 1], [1, 0, 1, 0]]
-    Solution.flipAndInvertImage(A)
+    Solution.flipAndInvertImage01(A)
+    Solution.flipAndInvertImage02(A)
 
 
 
